c0f47d1da63d3e80a6219cc3485921ee69bc495e
taha-elkholy <taha-elkholy>
2022-12-03 14:59:22 +0200

    set up Facebook & Google Configurations

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+
+// https://instagram-colon.firebaseapp.com/__/auth/handler
+
+//keytool -exportcert -alias androiddebugkey -keystore "C:\Users\taha\.android\debug.keystore" | "C:\openssl\bin\openssl" sha1 -binary | "C:\openssl\bin\openssl" base64
+      
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
